6b3558cc2cd6a052c33fb2b01d10925b3f2faab1
Shaik-Athik <shaikathik292007@gmail.com>
2026-09-11 23:25:59 +0530

    ✅ Find Numbers with Even Number of Digits | Easy | Python | Runtime: 4 ms (16.92%) | Memory: 19.3 MB | 2026-09-10

--- leetcode/easy/1295-find-numbers-with-even-number-of-digits/solution.py
+++ leetcode/easy/1295-find-numbers-with-even-number-of-digits/solution.py
@@ -1,7 +1,9 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         count = 0
+
         for x in nums:
-            if len(str(x))%2==0:
+            if len(str(x)) % 2 == 0:
                 count += 1
+        
         return count
