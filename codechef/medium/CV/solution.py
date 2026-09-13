580248a3c71979f97a4fc4f6dc72439a7a592abd
Shaik-Athik <shaikathik292007@gmail.com>
2026-09-13 23:30:43 +0530

    ✅ CV | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-13

---
+++ codechef/medium/CV/solution.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    count = 0
+    
+    for i in range(n-1):
+        if s[i] not in 'aeiou' and s[i+1] in 'aeiou':
+            count += 1
+    print(count)
