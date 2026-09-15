9fdae204251323a7eab22f51c150aadab9dce27c
Shaik-Athik <shaikathik292007@gmail.com>
2026-09-15 20:18:37 +0530

    ✅ Perfect Number | Easy | Python | Runtime: 3 ms (89.65%) | Memory: 19.1 MB | 2026-09-15

--- leetcode/easy/0507-perfect-number/solution.py
+++ leetcode/easy/0507-perfect-number/solution.py
@@ -1,9 +1,14 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        sum_ = 0
+        if num <= 1:
+            return False
+            
+        sum_ = 1
 
-        for i in range(1, int(num ** 0.5) + 1):
+        for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 sum_ += i
+                if i*i != num:
+                    sum_ += num // i
         
         return sum_ == num
