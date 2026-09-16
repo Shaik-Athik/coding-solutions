a8f23851b30580299f4a18c69b5473e20934015e
Shaik-Athik <shaikathik292007@gmail.com>
2026-09-16 19:22:31 +0530

    ✅ Single Number III | Medium | Python | Runtime: 3 ms (65.95%) | Memory: 20.8 MB | 2026-09-16

---
+++ leetcode/medium/0260-single-number-iii/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> list[int]:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        diff = xor & -xor
+
+        a, b = 0, 0
+
+        for num in nums:
+            if diff & num:
+                a ^= num
+            else:
+                b ^= num
+        
+        return [a,b]
